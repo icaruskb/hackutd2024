23090c938f0a33990c6d4affe09d95138b1e2919
trevrd22 <trevrd22@gmail.com>
2024-11-16 20:16:36 -0600

    wtf

--- src/app/DecodeImage/page.tsx
+++ src/app/DecodeImage/page.tsx
@@ -1,33 +1,6 @@
-"use client";
-
-import { SetStateAction, useState } from "react";
-import { PinataSDK } from "pinata";
-import Link from "next/link"
-
-const JWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIxM2UzZDlmMi01ZDhhLTRkODktYWU5Ny1hM2MyYzBlOTE1MTAiLCJlbWFpbCI6InJhcGhhZWxqY28wOUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJGUkExIn0seyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJOWUMxIn1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiNzc0MDNjNGNmZmY4NzU4NTE3MjUiLCJzY29wZWRLZXlTZWNyZXQiOiIzN2U1NGFiZjQxYTU5ZGQyZWUzMWVjYjE5OWNlNzkzMjcwYmMyMGJlYjRhZTllYWZkZWFjMDc0NmZkYjVmM2E0IiwiZXhwIjoxNzYzMzMxNjE4fQ.AAxb2vOqRvL5wjPxttjZ2cozwEVN59Hq0TKwQosZ_Q8";
-
-export default function DecodeImage() {
-
- return(
-  <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white p-8">
-      Enter your IPFS Hash
-      
-      <input className="text-black" type="text" placeholder="Enter text here" />
-      <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-          Burn
-        </button>
-
-      <Link href="./">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Authenticate 
-        </button>
-      
-        if response != cid 
-        return try again
-        else 
-        return the image
-        */
-      </Link>
-    </div>
- );
+import DecodeImage from "@/components/decodeImage";
+export default function decodeImagePage() {
+  return (
+    <DecodeImage />
+  );
 }
