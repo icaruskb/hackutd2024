0c6e71e2af9cb6e26591b64d43d590ae271f93e2
Raphael Co <raphaeljco09@gmail.com>
2024-11-16 23:17:11 -0600

    changed header to flexbox

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
-    <div>
-           <img src="/Cute Duck Detective Icon.webp" alt="Duck GIF" className="absolute top-0 left-0 m-4 cursor-pointer w-16 h-16"/>
-           <h2 className="absolute top-8 left-20 text-2xl font-bold text-white drop-shadow-lg">onetimeimages</h2>
+    <div className="flex items-center">
+           <img src="/Cute Duck Detective Icon.webp" alt="Duck GIF" className="m-4 cursor-pointer w-16 h-16"/>
+           <h2 className="text-2xl font-bold text-white drop-shadow-lg">onetimeimages</h2>
     </div>
   );
 };
