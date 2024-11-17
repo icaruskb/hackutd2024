476fcba141353c5bd351b3f543bf5eae7d667813
Raphael Co <raphaeljco09@gmail.com>
2024-11-16 23:19:11 -0600

    fixed flexbox for header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
-    <div className="flex items-center">
-           <img src="/Cute Duck Detective Icon.webp" alt="Duck GIF" className="m-4 cursor-pointer w-16 h-16"/>
+    <div className="flex items-center absolute top-0 left-0 m-4">
+           <img src="/Cute Duck Detective Icon.webp" alt="Duck GIF" className="cursor-pointer w-16 h-16"/>
            <h2 className="text-2xl font-bold text-white drop-shadow-lg">onetimeimages</h2>
     </div>
   );
